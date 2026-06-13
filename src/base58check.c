e3cb2986920d7d8ed6455779ae8524742026c101
tooru <tooru@tooru.us>
2026-06-14 01:15:12 +0300

    Initial commit

---
+++ src/base58check.c
@@ -0,0 +1,33 @@
+#include <string.h>
+#include "sha256.c"
+
+
+const char* BASE58_ALPHABET = "123456789"
+    "ABCDEFGHJKLMNPQRSTUVWXYZ"
+    "abcdefghijkmnopqrstuvwxyz";
+
+void base58(uint8_t s[], size_t s_size, char* out, size_t out_size) {
+    int c, i, n;
+
+    out[n = out_size] = 0;
+    while (n--) {
+        for (c = i = 0; i < s_size; i++) {
+            c = c * 256 + s[i];
+            s[i] = c / 58;
+            c %= 58;
+        }
+        out[n] = BASE58_ALPHABET[c];
+    }
+}
+
+void base58check(const uint8_t* data, size_t data_len, char* out, size_t out_size) {
+    uint8_t buffer[data_len + 4];
+    memcpy(buffer, data, data_len);
+
+    uint8_t checksum[32];
+    sha256(data_len, data, checksum);
+    sha256(32, checksum, checksum);
+
+    memcpy(buffer + data_len, checksum, 4);
+    base58(buffer, data_len + 4, out, out_size);
+}
---
+++ src/vanity.c
@@ -0,0 +1,173 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <secp256k1.h>
+#include "Keccak.c"
+#include "base58check.c"
+
+#define PRIVKEY_LEN 32
+#define PUBKEY_LEN 65
+#define ADDRESS_LEN 21
+#define B58CHECK_ADDRESS_LEN 34
+#define HELP_TEXT "Usage:\n" \
+                  "  %s [-i] pattern\n" \
+                  "\n" \
+                  "Options:\n" \
+                  "  -i            Case-insensitive search\n" \
+                  "  -h, --help    Print help\n"
+
+secp256k1_context* ctx;
+
+void error_exit(const char* message) {
+    fprintf(stderr, "%s\n", message);
+    if (ctx) secp256k1_context_destroy(ctx);
+    exit(EXIT_FAILURE);
+}
+
+void print_hex(size_t len, const byte data[static len]) {
+    for (size_t i = 0; i < len; i++) {
+        printf("%02x", data[i]);
+    }
+    putchar('\n');
+}
+
+bool privkey_to_pubkey(const byte privkey[static PRIVKEY_LEN], byte pubkey[static PUBKEY_LEN]) {
+    secp256k1_pubkey pub;
+
+    if (!secp256k1_ec_pubkey_create(ctx, &pub, privkey)) {
+        return false;
+    }
+
+    size_t pubkey_len = PUBKEY_LEN;
+    secp256k1_ec_pubkey_serialize(ctx, pubkey, &pubkey_len, &pub, SECP256K1_EC_UNCOMPRESSED);
+    return true;
+}
+
+void pubkey_to_tron_address(const byte pubkey[static PUBKEY_LEN], byte address[static ADDRESS_LEN]) {
+    byte hash[PRIVKEY_LEN];
+    Keccak_256(pubkey + 1, PUBKEY_LEN - 1, hash);
+    address[0] = 0x41;
+    memcpy(address + 1, hash + PRIVKEY_LEN - 20, 20);
+}
+
+bool privkey_to_tron_address(const byte privkey[static PRIVKEY_LEN], byte address[static ADDRESS_LEN]) {
+    byte pubkey[PUBKEY_LEN];
+    if (!privkey_to_pubkey(privkey, pubkey)) {
+        return false;
+    }
+
+    pubkey_to_tron_address(pubkey, address);
+    return true;
+}
+
+NTSTATUS generate_private_key(byte buf[static PRIVKEY_LEN]) {
+    return BCryptGenRandom(NULL, buf, PRIVKEY_LEN, BCRYPT_USE_SYSTEM_PREFERRED_RNG);
+}
+
+void print_address_info(const char* b58check_address, const byte privkey[static PRIVKEY_LEN], unsigned long long count) {
+    printf("Address: %s\nPrivate Key: ", b58check_address);
+    print_hex(PRIVKEY_LEN, privkey);
+    printf("Count: %llu\n", count);
+}
+
+bool matches_pattern(const char* address, const char* pattern, bool case_sensitive) {
+    if (address[0] != 'T') return false;
+
+    const char* addr_pos = address + 1;
+    const char* pat_pos = pattern;
+
+    while (*pat_pos) {
+        if (!*addr_pos) return false;
+
+        char addr_char = *addr_pos;
+        char pat_char = *pat_pos;
+
+        if (!case_sensitive) {
+            addr_char = toupper((unsigned char)addr_char);
+            pat_char = toupper((unsigned char)pat_char);
+        }
+
+        if (addr_char != pat_char) return false;
+
+        addr_pos++;
+        pat_pos++;
+    }
+
+    return true;
+}
+
+bool validate_pattern(const char* pattern) {
+    return strspn(pattern, BASE58_ALPHABET) == strlen(pattern);
+}
+
+char* parse_args(int argc, char * argv[], bool* cs) {
+    if (argc > 3) {
+        error_exit("Too many arguments.");
+    }
+
+    if (argc == 1) {
+        printf(HELP_TEXT, argv[0]);
+        exit(EXIT_SUCCESS);
+    }
+
+    *cs = true;
+    char* pattern = NULL;
+
+    for (int i = 1; i < argc; ++i) {
+        if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+            printf(HELP_TEXT, argv[0]);
+            exit(EXIT_SUCCESS);
+        } else if (strcmp(argv[i], "-i") == 0) {
+            *cs = false;
+        } else if (*argv[i] == '-') {
+            error_exit("Unrecognized option.");
+        } else if (pattern != NULL) {
+            error_exit("Only one pattern can be specified.");
+        } else if (!validate_pattern(pattern = argv[i])) {
+            error_exit("Pattern must a subset of Base58 alphabet.");
+        }
+    }
+
+    if (pattern == NULL) {
+        error_exit("You must specify a pattern to look for.");
+    }
+
+    return pattern;
+}
+
+
+int main(int argc, char* argv[]) {
+    bool case_sensitive;
+    char* pattern = parse_args(argc, argv, &case_sensitive);
+
+    ctx = secp256k1_context_create(SECP256K1_CONTEXT_NONE);
+    byte privkey[PRIVKEY_LEN], address[ADDRESS_LEN];
+
+    unsigned long long count = 0;
+    char b58check_address[B58CHECK_ADDRESS_LEN + 1];
+
+    while (!matches_pattern(b58check_address, pattern, case_sensitive)) {
+        count++;
+
+        if (!BCRYPT_SUCCESS(generate_private_key(privkey))) {
+            error_exit("Failed to generate randomness.");
+        }
+
+        if (!privkey_to_tron_address(privkey, address)) {
+            error_exit("Error computing public key.");
+        }
+
+        base58check(address, ADDRESS_LEN, b58check_address, B58CHECK_ADDRESS_LEN);
+
+        if (count % 100000 == 0) {
+            printf("%llu wallets checked\r", count);
+        }
+    };
+
+    print_address_info(b58check_address, privkey, count);
+
+    secp256k1_context_destroy(ctx);
+}
